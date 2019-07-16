66d15a910921b805d04c0311a38ee00da4abaeba
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-07-16 11:47:42 -0700

    Setup almost done

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../css/App.css';
+import Messages from './Messages'
+import Message from './Message'
+import Toolbar from './Toolbar'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App
